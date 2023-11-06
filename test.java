295646e2cbe68aefb5f5703131fbbf4bfe8c113c
JongKook <94429120+JongKookE@users.noreply.github.com>
2023-11-06 12:06:06 +0900

    Delete test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        double random = Math.random();
-        int value = (int) (random * 100) + 1;
-        System.out.println(value);
-    }
-}
