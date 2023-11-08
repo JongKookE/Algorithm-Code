71a0858d71c50ee5b726822f5146a6b3eb71502f
jongkook <whdrnrdl789@naver.com>
2023-11-08 14:02:42 +0900

    보석도둑

--- greedy/BOJ_1202_보석_도둑.java
+++ greedy/BOJ_1202_보석_도둑.java
@@ -3,24 +3,37 @@ package greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_1202_보석_도둑 {
-	static int jewerly, bag;
-	static PriorityQueue<Node> pq = new PriorityQueue<>();
+	static int jewerly, bagSize, result;
+	static int[] bag;
+//	static PriorityQueue<Node> pq = new PriorityQueue<>();
+	static Node[] nodes;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		jewerly = Integer.parseInt(st.nextToken());
-		bag = Integer.parseInt(st.nextToken());
+		bagSize = Integer.parseInt(st.nextToken());
+		bag = new int[bagSize];
+		nodes = new Node[jewerly];
 		
 		for(int i = 0; i < jewerly; i++) {
 			st = new StringTokenizer(br.readLine());
 			int weight = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			pq.add(new Node(weight, cost));
+			nodes[i] = new Node(weight, cost);
 		}
+		
+		for(int b = 0; b < bagSize; b++) bag[b]= Integer.parseInt(br.readLine());
+		
+		
+		
+		System.out.println(result);
+		
 	}
 	static class Node implements Comparable<Node>{
 		int weight, cost;
@@ -38,8 +51,8 @@ public class BOJ_1202_보석_도둑 {
 
 		@Override
 		public int compareTo(Node o) {
-			if(this.weight == o.weight) return this.cost - o.cost;
-			else return this.weight - o.weight;
+			if(o.cost == this.cost) return this.weight - o.weight;
+			return o.cost - this.cost;
 		}
 		
 		
