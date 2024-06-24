0f2ed1ed91a61ca9b02db8b69966f4b055af7c53
JongKook <whdrnrdl789@naver.com>
2024-06-24 16:17:43 +0900

    2024.06.24

--- simulation/BOJ_5430_AC.java
+++ simulation/BOJ_5430_AC.java
@@ -16,14 +16,14 @@ public class BOJ_5430_AC {
         T = Integer.parseInt(br.readLine());
         for(int t = 0; t < T; t++){
             String command = br.readLine();
-            int count = Integer.parseInt(br.readLine());
-            String temp = br.readLine();
-            char[] data = new char[temp.length() - 2];
-            for(int d =0; d < temp.length() - 2; d++) data[d] = temp.charAt(d+1);
+            int arrayLength = Integer.parseInt(br.readLine());
+            String dataString = br.readLine();
+            char[] data = dataString.substring(1, dataString.length()-1).toCharArray();
             StringTokenizer st = new StringTokenizer(new String(data), ",");
 
             while(st.hasMoreTokens()) dq.addLast(Integer.parseInt(st.nextToken()));
             boolean dir = true;
+            boolean isEnd = true;
             for(int c = 0; c < command.length(); c++){
                 char mission = command.charAt(c);
                 switch (mission) {
@@ -33,24 +33,27 @@ public class BOJ_5430_AC {
                     // true 라면 정방향
                     // false 라면 역방향
                     case 'D' -> {
+                        if(dq.isEmpty()) {
+                            sb.append("error").append("\n");
+                            isEnd = false;
+                            break;
+                        }
                         if(dir) dq.pollFirst();
                         else dq.pollLast() ;
                     }
                 }
+                if(!isEnd) break;
             }
-
-            if(dq.isEmpty()) sb.append("error").append("\n");
-            else if (dir){
-                sb.append("[");
-                while(dq.size() != 1) sb.append(dq.pollFirst()).append(",");
-                sb.append(dq.pollFirst()).append("]").append("\n");
+            if(!isEnd) continue;
+            sb.append("[");
+            while(!dq.isEmpty()) {
+                if (dir) sb.append(dq.pollFirst()).append(",");
+                else sb.append(dq.pollLast()).append(",");
             }
-            else{
-                sb.append("[");
-                while(dq.size() != 1) sb.append(dq.pollLast()).append(",");
-                sb.append(dq.pollLast()).append("]").append("\n");
-            }
+            if(sb.charAt(sb.length()-1) == ',') sb.setLength(sb.length()-1);
+            sb.append("]").append("\n");
         }
         System.out.println(sb);
     }
 }
+
