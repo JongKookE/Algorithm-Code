0f9acacca828d4b0f934fbb18d2e144ca7b1ccc9
jongkookE <whdrnrdl789@naver.com>
2024-07-09 20:21:46 +0900

    [Bronze II] Title: 분해합, Time: 272 ms, Memory: 50796 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            String str = String.valueOf(i);
+            if(N == calculate(str)){
+                System.out.println(str);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+    static int calculate(String str) {
+        int size = str.length();
+        int sum = Integer.parseInt(str);
+        for(int i = 0; i < size; i++) sum += str.charAt(i) - '0';
+        return sum;
+    }
+}
